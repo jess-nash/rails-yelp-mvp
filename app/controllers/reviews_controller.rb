0580ca333ef191b41443d656c335891162b255b8
Jessica Nash <jeenash2@gmail.com>
2022-07-06 20:17:26 +0900

    MVC working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
